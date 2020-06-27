5aca742d138868f38893328d1c1ef8d351d3b61a
Colin Campbell <colincampbell321123@hotmail.com>
2020-06-27 17:07:31 +0100

    Add tests at root level to check for invalid arguments

--- test.js
+++ test.js
@@ -30,4 +30,12 @@ describe("pow", function () {
 
     });
 
+    it("for negative n the result is NaN", function () {
+        assert.isNaN(pow(2, -1));
+    });
+
+    it("for non-integer n the result is NaN", function () {
+        assert.isNaN(pow(2, 1.5));
+    });
+
 });
