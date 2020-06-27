4237468641d64fba90466532720725166a7b4357
Colin Campbell <colincampbell321123@hotmail.com>
2020-06-27 17:01:34 +0100

    Generate tests

--- test.js
+++ test.js
@@ -1,11 +1,14 @@
 describe("pow", function () {
 
-    it("2 raised to power 3 is 8", function () {
-        assert.equal(pow(2, 3), 8);
-    });
+    function makeTest(x) {
+        let expected = x * x * x;
+        it(`${x} in the power 3 is ${expected}`, function () {
+            assert.equal(pow(x, 3), expected);
+        });
+    }
 
-    it("3 raised to power 4 is 81", function () {
-        assert.equal(pow(3, 4), 81);
-    });
+    for (let x = 1; x <= 5; x++) {
+        makeTest(x);
+    }
 
 });
