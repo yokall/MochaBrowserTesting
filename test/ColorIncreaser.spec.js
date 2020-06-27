5841cafc47af30d93fa7644f0ce676e1e59e9370
Colin Campbell <colincampbell321123@hotmail.com>
2020-06-27 17:33:22 +0100

    Test class Fixes #3
    Introduce actual app html page

---
+++ test/ColorIncreaser.spec.js
@@ -0,0 +1,84 @@
+function mockColor(red, green, blue, alpha) {
+    // Mock of the color class from p5
+    this.levels = [];
+    this.levels[0] = red;
+    this.levels[1] = green;
+    this.levels[2] = blue;
+    this.levels[3] = alpha;
+}
+
+describe('ColorIncreaser tests', function () {
+    // Will hold the reference to the ColorIncreaser class
+    let colorIncreaser;
+
+    // beforeEach is a special function that is similar to the setup function in
+    // p5.js.  The major difference it that this function runs before each it()
+    // test you create instead of running just once before the draw loop
+    // beforeEach lets you setup the objects you want to test in an easy fashion.
+    beforeEach(function () {
+        let colorValueIncrease = 1;
+        let fillColor = new mockColor(0, 0, 0, 255);
+        colorIncreaser = new ColorIncreaser(colorValueIncrease, fillColor);
+    });
+
+    it('should be an object', function (done) {
+        expect(colorIncreaser).to.be.a('object');
+        done();
+    });
+
+    it('should store initial values without mutation', function (done) {
+        expect(colorIncreaser.colorValueIncrease).to.be.equal(1);
+        expect(colorIncreaser.fillColor.levels[0]).to.equal(0)
+        expect(colorIncreaser.fillColor.levels[1]).to.equal(0)
+        expect(colorIncreaser.fillColor.levels[2]).to.equal(0)
+        expect(colorIncreaser.fillColor.levels[3]).to.equal(255);
+        done();
+    });
+
+    it('should have rgb values 255, 0, 0 after calling increaseFillColor 255 times', function (done) {
+        //it is 256^1 - 1 because it starts with the color black
+        for (let count = 0; count < 255; count += 1) {
+            colorIncreaser.increaseFillColor()
+        }
+        expect(colorIncreaser.fillColor.levels[0]).to.equal(255)
+        expect(colorIncreaser.fillColor.levels[1]).to.equal(0)
+        expect(colorIncreaser.fillColor.levels[2]).to.equal(0)
+        done();
+    });
+
+
+    it('should have rgb values 255, 255, 0 after calling increaseFillColor 65535 times', function (done) {
+        //it is 256^2 - 1 because it starts with the color black
+        for (let count = 0; count < 65535; count += 1) {
+            colorIncreaser.increaseFillColor()
+        }
+        expect(colorIncreaser.fillColor.levels[0]).to.equal(255)
+        expect(colorIncreaser.fillColor.levels[1]).to.equal(255)
+        expect(colorIncreaser.fillColor.levels[2]).to.equal(0)
+        done();
+    });
+
+
+    it('should have rgb values 255, 255, 255 after calling increaseFillColor 16777215 times', function (done) {
+        //it is 256^3 - 1 because it starts with the color black
+        for (let count = 0; count < 16777215; count += 1) {
+            colorIncreaser.increaseFillColor()
+        }
+        expect(colorIncreaser.fillColor.levels[0]).to.equal(255)
+        expect(colorIncreaser.fillColor.levels[1]).to.equal(255)
+        expect(colorIncreaser.fillColor.levels[2]).to.equal(255)
+        done();
+    });
+
+
+    it('should have rgb values 0, 0, 0 after calling increaseFillColor 16777216 times', function (done) {
+        //it is 256^3 because it starts with the color black
+        for (let count = 0; count < 16777216; count += 1) {
+            colorIncreaser.increaseFillColor()
+        }
+        expect(colorIncreaser.fillColor.levels[0]).to.equal(0)
+        expect(colorIncreaser.fillColor.levels[1]).to.equal(0)
+        expect(colorIncreaser.fillColor.levels[2]).to.equal(0)
+        done();
+    });
+});
