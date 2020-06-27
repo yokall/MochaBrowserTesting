e552a410209570e04f76615f5b5589acae658856
Colin Campbell <colincampbell321123@hotmail.com>
2020-06-27 17:04:00 +0100

    Group tests using nested describes

--- test.js
+++ test.js
@@ -1,14 +1,33 @@
 describe("pow", function () {
 
-    function makeTest(x) {
-        let expected = x * x * x;
-        it(`${x} in the power 3 is ${expected}`, function () {
-            assert.equal(pow(x, 3), expected);
-        });
-    }
+    describe("raises x to power 2", function () {
 
-    for (let x = 1; x <= 5; x++) {
-        makeTest(x);
-    }
+        function makeTest(x) {
+            let expected = x * x;
+            it(`${x} in the power 2 is ${expected}`, function () {
+                assert.equal(pow(x, 2), expected);
+            });
+        }
+
+        for (let x = 1; x <= 5; x++) {
+            makeTest(x);
+        }
+
+    });
+
+    describe("raises x to power 3", function () {
+
+        function makeTest(x) {
+            let expected = x * x * x;
+            it(`${x} in the power 3 is ${expected}`, function () {
+                assert.equal(pow(x, 3), expected);
+            });
+        }
+
+        for (let x = 1; x <= 5; x++) {
+            makeTest(x);
+        }
+
+    });
 
 });
